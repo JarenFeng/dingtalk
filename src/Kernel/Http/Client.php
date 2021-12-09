97fa57c69e6221b027c3b3a5ee46b6d7c1c3c1b9
冯景润 <im@jaren.im>
2021-12-09 11:52:52 +0800

    Update Client.php
    修复Http/Client.php中没有get()和postJSON的问题

--- src/Kernel/Http/Client.php
+++ src/Kernel/Http/Client.php
@@ -101,4 +101,36 @@ class Client extends BaseClient
 
         return $this;
     }
+    
+    /**
+     * GET request.
+     *
+     * @param string $url
+     * @param array $query
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Overtrue\Http\Support\Collection|array|object|string
+     */
+    public function get(string $url, array $query = [], $async = false)
+    {
+        return $this->request($url, 'GET', ['query' => $query], $async);
+    }
+
+    /**
+     * JSON request.
+     *
+     * @param string $url
+     * @param string|array $data
+     * @param array $query
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Overtrue\Http\Support\Collection|array|object|string
+     */
+    public function postJson(string $url, array $data = [], array $query = [])
+    {
+        return $this->request($url, 'POST', ['query' => $query, 'json' => $data]);
+    }
+}
 }
